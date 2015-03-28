162cabc44b5362afc8d7a4421a68dba395f035e7
ShiJianwen <shijianwen95@gmail.com>
2015-03-28 15:12:58 +0800

    修改接口

--- style/js/style.js
+++ style/js/style.js
@@ -1,45 +1,46 @@
 	var User = {};  //用户个人信息对象
 	var TeacherList = [];  //被评价人列表
-	var Host = 'http://10.10.106.150:8080/jeecg-framework/'; 
+	var Host = 'http://172.25.188.1:8080/jeecg-framework/';  //定义接口域名
+
 	//登录ajax请求
-	$('#login-btn').click(function(){
+	// $('#login-btn').click(function(){
 
-		User.userId = $('#username').val();
-		User.userPW = $('#password').val();
-		console.log(User);
+	// 	User.username = $('#username').val();
+	// 	User.password = $('#password').val();
+	// 	console.log(JSON.stringify(User));
 
-		$.ajax({  
-			headers: { 
-			        'Accept': 'application/json',
-			        'Content-Type': 'application/json' 
-			    },
-			url: Host + "weChatController.do?/login",
-			type : "POST",
+		// $.ajax({  
+			// headers: { 
+			        // 'Accept': 'application/json',
+			        // 'Content-Type': 'application/json' 
+			    // },
+			// url: Host + "weChatController.do?login",
+			// url : '127.0.0.1:8080/EVA/login.php',
+			// type : "POST",
 			// crossDomain: true,
-			"dataType" : "json",
-			"data" : {
-				"username" : User.userId,
-				"password" : User.userPW
-			},
-			complete : function(data){
-				console.log(data.status);
-				if(data.status == '200') {  //登录成功时获取个人信息及被评价人列表
-					console.log(data);
-					$.ajax({
-						headers: { 
-						        'Accept': 'application/json',
-						        'Content-Type': 'application/json' 
-						    },
-						url: Host+"weChatController.do?getMyPersonInfo",
-						"type" : "GET",
-						"dataType" : "json",
-						"data" : {
-							"username" : User.userId
-						},
-						complete: function(data) {
-							console.log(data.status);
-							if(data.status == '200') {
-								console.log(data);
+			// dataType : "json",
+			// data : JSON.stringify(User),
+			// complete : function(data){
+			// 	console.log(data.status);
+			// 	if(data.status == '200') {  //登录成功时获取个人信息及被评价人列表
+			// 		console.log(data);
+			// 		window.location = '/EVA/index.html'
+					// $.ajax({
+					// 	headers: { 
+					// 	        'Accept': 'application/json',
+					// 	        'Content-Type': 'application/json' 
+					// 	    },
+					// 	url: Host+"weChatController.do?getMyPersonInfo",
+					// 	type : "GET",
+					// 	dataType : "json",
+					// 	data : {
+					// 		"username" : User.userId
+					// 	},
+					// 	complete: function(data) {
+					// 		console.log(data.status);
+					// 		if(data.status == '200') {
+					// 			console.log(data);
+								// window.location = '/EVA/index.html';
 								// User.userId = data.username;
 								// User.userName = data.user_Autonym;
 								// User.userType = data.user_Type;
@@ -48,33 +49,61 @@
 									// $('#TeacherInfo').append('<tr><td>'+this.teacherName+'</td><td>'+this.teacherNo+'</td><td>'+this.teacherType+'</td></tr>');
 									// TeacherList.push({"teacherName":this.teacherName,"teacherNo":this.teacherNo,"teacherType":this.teacherType });
 								// });
-							}
-						}
-					});
-				}
+					// 		}
+					// 	}
+					// });
+				// }
 			 // : function(error) {
-			 	if(data.status == '400') {
-			 		// alert('fuwuqi');
-			 		$('form').append('服务器错误');
-			 	}
-				if(data.status == '404') {
-					$('form').append('用户不存在！');
+		// 	 	if(data.status == '400') {
+		// 	 		// alert('fuwuqi');
+		// 	 		$('form').append('服务器错误');
+		// 	 	}
+		// 		if(data.status == '404') {
+		// 			$('form').append('用户不存在！');
 
-				}
-				if(data.status == '401') {
-					$('form').append('密码错误！');
+		// 		}
+		// 		if(data.status == '401') {
+		// 			$('form').append('密码错误！');
 
-				}
-				if(data.status == '403') {
-					$('form').append('禁止访问！');
+		// 		}
+		// 		if(data.status == '403') {
+		// 			$('form').append('禁止访问！');
 
-				}
+		// 		}
 
-			}
-		});
-		});
+		// 	}
+		// });
+		// });
 
 	//获取用户个人信息及被评价人列表ajax请求
+
+	var getUserInfo = function () {
+		$.ajax({
+				headers: { 
+				        'Accept': 'application/json',
+				        'Content-Type': 'application/json' 
+				    },
+				url: Host+"weChatController.do?getMyPersonInfo",
+				type : "GET",
+				dataType : "json",
+				data : {
+					"username" : '3113005732'
+				},
+				complete: function(data) {
+					console.log(data.status);
+					if(data.status == '200') {
+						console.log(data);
+						// window.location = '/EVA/index.html';
+						// User.userId = data.username;
+						// User.userName = data.user_Autonym;
+						// User.userType = data.user_Type;
+						// User.userSystenType = data.user_Roles;
+					}
+				}
+			});
+
+	};
+
 	$('#getMyPersonalInfo').click(function(){	
 							
 					//获取个人信息并渲染页面
@@ -87,6 +116,7 @@
 						$('#TeacherInfo').append('<tr><td>'+this.teacherName+'</td><td>'+this.teacherNo+'</td><td>'+this.teacherType+'</td></tr>');
 					});
 	});
+
 
 	//打开评价页面时填充被评价人信息
 	$('#getComments').click(function(){
@@ -131,5 +161,3 @@
 				}
 			});
 	});
-
-
